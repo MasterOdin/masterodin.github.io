88241169112941791fb6686191de5d2b64b59919
Matthew Peveler <matt.peveler@gmail.com>
2016-06-24 18:02:11 -0400

    fix utf-8 characters

--- index.php
+++ index.php
@@ -5,14 +5,6 @@ $pages = array(
 				'work',
 				'resume',
 			);
-
-$projects = array(
-					'turing',
-					'cfg2cnf',
-					'flynn',
-					'alt',
-				);
-
 /* end init */
 
 $content = !isset($_GET['content']) || !in_array($_GET['content'],$pages) ? 'home' : $_GET['content'];
@@ -23,16 +15,8 @@ $content = !isset($_GET['content']) || !in_array($_GET['content'],$pages) ? 'hom
 <head>
 	<title>matthew peveler - <?php print $content; ?></title>
 	<link rel="stylesheet" href="assets/style.css" type="text/css" />
-
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-		ga('create', 'UA-40645452-1', 'mpeveler.com');
-		ga('send', 'pageview');
-	</script>
+	<meta charset="UTF-8">
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8">
 </head>
 <body>
 <!-- header -->
@@ -42,7 +26,7 @@ $content = !isset($_GET['content']) || !in_array($_GET['content'],$pages) ? 'hom
 			<ul>
 				<li<?php print ($content == "home") ? ' class="selected"' : ""; ?>><a href="?content=home">Home</a></li>
 				<li<?php print ($content == "work") ? ' class="selected"' : ""; ?>><a href="?content=work">Work</a></li>
-				<li<?php print ($content == "resume") ? ' class="selected"' : ""; ?>><a href="?content=resume">Résumé</a></li>
+				<li<?php print ($content == "resume") ? ' class="selected"' : ""; ?>><a href="?content=resume">RÃ©sumÃ©</a></li>
 			</ul>
 		</div>
 		<h1><a href="?content=home">matthew peveler</a></h1>
@@ -50,12 +34,7 @@ $content = !isset($_GET['content']) || !in_array($_GET['content'],$pages) ? 'hom
 
 	<div id="content">
 <?php
-	if($content == "work" && (isset($_GET['proj']) && in_array($_GET['proj'], $projects)))
-	{
-		require_once("assets/content/projects/".$_GET['proj'].".html");
-	} else {
-		require_once("assets/content/".$content.".html");
-	}
+	require_once("assets/content/".$content.".html");
 ?>
 	</div>
 
